705c97e6feb46c480431fd8ace77917763b270da
Vicly Cao <v.cao@celayix.com>
2023-07-21 12:40:52 -0700

    fix email issues

--- amplify/backend/function/emailHandler/src/index.js
+++ amplify/backend/function/emailHandler/src/index.js
@@ -12,6 +12,7 @@ exports.handler = async (event) => {
       const info = record.dynamodb.NewImage;
       const time = new Date(parseInt(info.time.N));
       const appLength = info.type.S === '90' ? '90 Minutes' : '1 Hour';
+      const emailBodyText = `Client: ${info.client.S}\nTime (Pacific Timezone): ${time.toLocaleString('en-US', { timeZone: "America/Los_Angeles", year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true })}\nLength: ${appLength}\nAdditional notes: ${info.note.S || 'None'}`;
       await SES
         .sendEmail({
           Destination: {
@@ -22,11 +23,8 @@ exports.handler = async (event) => {
             Subject: { Data: 'New massage appointment created' },
             Body: {
               Text: {
-                Data: `Client: ${info.client.S}\n
-            Time: ${time.toLocaleString([], { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true })}\n
-            Length: ${appLength}\n
-            Additional notes: ${info.note.S || 'None'}
-            ` },
+                Data: emailBodyText
+              }
             },
           },
         })
@@ -42,11 +40,8 @@ exports.handler = async (event) => {
             Subject: { Data: 'New massage appointment created' },
             Body: {
               Text: {
-                Data: `Client: ${info.client.S}\n
-            Time: ${time.toLocaleString([], { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true })}\n
-            Length: ${appLength}\n
-            Additional notes: ${info.note.S || 'None'}
-            ` },
+                Data: emailBodyText  
+              },
             },
           },
         })
@@ -57,6 +52,11 @@ exports.handler = async (event) => {
       const appLength = info.type.S === '90' ? '90 Minutes' : '1 Hour';
       const oldInfo = record.dynamodb.OldImage;
       const oldTime = new Date(parseInt(oldInfo.time.N));
+      const emailBodyText = info.time.N === oldInfo.time.N ?
+        `Client: ${info.client.S}\nPrevious Time (Pacific Timezone): ${oldTime.toLocaleString('en-US', { timeZone: "America/Los_Angeles", year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true })}\nUpdated Time (Pacific Timezone): Time unchanged\nLength: ${appLength}\nAdditional notes: ${info.note.S || 'None'}`
+        :
+        `Client: ${info.client.S}\nPrevious Time (Pacific Timezone): ${oldTime.toLocaleString('en-US', { timeZone: "America/Los_Angeles", year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true })}\nUpdated Time (Pacific Timezone): ${time.toLocaleString('en-US', { timeZone: "America/Los_Angeles", year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true })}\nLength: ${appLength}\nAdditional notes: ${info.note.S || 'None'}`
+
       await SES
         .sendEmail({
           Destination: {
@@ -67,12 +67,8 @@ exports.handler = async (event) => {
             Subject: { Data: 'Massage appointment updated' },
             Body: {
               Text: {
-                Data: `Client: ${info.client.S}\n
-            Previous Time: ${oldTime.toLocaleString([], { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true })}\n
-            Updated time: ${time.toLocaleString([], { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true })}\n
-            Length: ${appLength}\n
-            Additional notes: ${info.note.S || 'None'}
-            ` },
+                Data: emailBodyText
+              }
             },
           },
         })
@@ -88,12 +84,8 @@ exports.handler = async (event) => {
             Subject: { Data: 'Massage appointment updated' },
             Body: {
               Text: {
-                Data: `Client: ${info.client.S}\n
-            Previous Time: ${oldTime.toLocaleString([], { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true })}\n
-            Updated time: ${time.toLocaleString([], { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true })}\n
-            Length: ${appLength}\n
-            Additional notes: ${info.note.S || 'None'}
-            ` },
+                Data: emailBodyText
+              }
             },
           },
         })
@@ -101,6 +93,7 @@ exports.handler = async (event) => {
     } else if (record.eventName === 'REMOVE') {
       const oldInfo = record.dynamodb.OldImage;
       const oldTime = new Date(parseInt(oldInfo.time.N));
+      const emailBodyText = `Client: ${oldInfo.client.S}\nPrevious Time (Pacific Timezone): ${oldTime.toLocaleString('en-US', { timeZone: "America/Los_Angeles", year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true })}`
       await SES
         .sendEmail({
           Destination: {
@@ -111,12 +104,12 @@ exports.handler = async (event) => {
             Subject: { Data: 'Massage appointment cancelled' },
             Body: {
               Text: {
-                Data: `Client: ${oldInfo.client.S}\n
-            Previous Time: ${oldTime.toLocaleString([], { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true })}\n
-            ` },
+                Data: emailBodyText
+              },
             },
           },
         })
+        .promise()
         await delay(2000)
         await SES
         .sendEmail({
@@ -128,12 +121,12 @@ exports.handler = async (event) => {
             Subject: { Data: 'Massage appointment cancelled' },
             Body: {
               Text: {
-                Data: `Client: ${oldInfo.client.S}\n
-            Previous Time: ${oldTime.toLocaleString([], { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true })}\n
-            ` },
+                Data: emailBodyText
+              },
             },
           },
         })
+        .promise()
     }
 
   }
